bf2da9fc96f668d8a7134a66f744463e6e0da802
Mary Clapp <mkclapp@ucdavis.edu>
2019-01-21 13:26:09 -0800

    new batexplore code

--- scripts/batexplore_20180410.R
+++ scripts/batexplore_20180410.R
@@ -12,17 +12,6 @@ library(tidyverse)
 library(lubridate)
 library(chron)
 
-# READ IN, CLEAN UP SONOVET OUTPUT FILES ----------------------------------
-
-allsites <- list.files ("data", full.names = TRUE, pattern = ".txt")
-t <- do.call("rbind", lapply(X = allsites, FUN = read_tsv, col_names = FALSE, skip = 1)) #t for 'total' sites
-# TO DO: check the above to make sure all the files bound correctly-- the code above returns warnings
-# always take a glimpse to make sure your data are read in correctly
-glimpse(t)
-
-# a1 <- read_tsv("data/AMPHIT1_July_shorts_output.txt", col_names=TRUE) #AMPHIT1 ONLY
-# summary(a1)
-
 
 # TO-DO LIST --------------------------------------------------------------
 ## UPDATE COLUMN NAMES NOW 04242018 //
@@ -37,9 +26,19 @@ glimpse(t)
 ## FEEDBACK FROM LEILA REGARDING HI=1&LO=1 EXAMPLES //?
 ## FIX GROUP_BY(CHECKME)? // <- decided unimportant
 ## Leila: are our AccpSpp actually good enough to use?
-## Leila: SonoBat license?
+## Leila: SonoBat license? <-- ACQUIRED! Leila is a collaborator!
 
-# COLUMN NAMES ------------------------------------------------------------
+# READ IN, CLEAN UP SONOVET OUTPUT FILES ----------------------------------
+
+# output files from Madi, ~spring 2017
+allsites <- list.files ("data", full.names = TRUE, pattern = ".txt")
+t <- do.call("rbind", lapply(X = allsites, FUN = read_tsv, col_names = FALSE, skip = 1)) #t for 'total' sites
+# TO DO: check the above to make sure all the files bound correctly-- the code above returns warnings
+# always take a glimpse to make sure your data are read in correctly
+glimpse(t)
+
+# a1 <- read_tsv("data/AMPHIT1_July_shorts_output.txt", col_names=TRUE) #AMPHIT1 ONLY
+# summary(a1)
 
 # rename columns to get rid of symbols...fix this later when we know true column significances
 names(t) <- c("Filename", "HiF", "LoF", "ManualID", "AccpSpp", "Maj", "Accp", "TildeSpp", "Primary", "Secondary", "Tert", "Quat",
@@ -123,23 +122,71 @@ class(t$Time)
 t <- t %>%
   mutate(hr = substr(Time, 1,2))
 
+
+# NEW data 1/25/2019 from Leila -------------------------------------------
+
+t <- read_csv(file = "./data/Shorts_Combined_CumSonoBatch_v430.csv")
+glimpse(t)
+# change columns with numbers to class 'numeric'
+t[,11:20] <- lapply(t[,11:20], as.numeric) # 'lapply' is taking columns 13-23 of the t dataframe and applying the function 'as.numeric' to them
+
+summary(t) #confirm that it worked-- summary can now return summary stats on the numeric columns
+t[,32:34] <- lapply(t[,32:34], as.numeric)
+
+# TEMPORARY potential solution-- changing NA's to 0's 
+t[is.na(t)] <- 0
+
+# DATE/TIME FORMATTING ----------------------------------------------------
+# create columns for date and time using the 'separate' function to split the latent info in the Filename column
+# the second 'separate' command is a janky way to do this but i don't feel like learning regular expressions rn
+t <- t %>% 
+  separate(Filename, c("Site", "Date", "Time"), sep = "_", remove = TRUE) %>%
+  separate(Time, c("Time", "fileext"), sep=".w")
+t$fileext <- NULL # this deletes this column
+
+# create a Timestamp column because it's easy to coerce to the date/time class
+t <- t %>% 
+  unite(Timestamp, Date, Time, sep = " ", remove = FALSE)
+class(t$Timestamp) 
+
+# format Timestamp to correct class
+t$Timestamp <- ymd_hms(t$Timestamp)
+class(t$Timestamp) #check class now, should be POSIXct
+
+# FINALLY create separate columns for date and time and coerce to date/time class
+t <- t %>%
+  separate(Timestamp, c("Date", "Time"), sep = " ", remove = FALSE)
+lapply(t, class) # returns class for all columns
+t$Date <- ymd(t$Date)
+t$Time <- chron(times=t$Time)
+class(t$Time)
+
+# add column for HOUR (to later investigate call activity by hour of night)
+t <- t %>%
+  mutate(hr = substr(Time, 1,2))
+
+
 # FILTER DATA -------------------------------------------------------------
 # filter out all non-bat entries and create a new dataframe called 'detect' with only entries that definitely contain bat passes
 # from SonoBat documentation: "For rudimentary tallying of bat passes, SonoBat separates bat species into low and high frequency acoustic clades. Even if unable to perform a species identification, if recognized as a bat, SonoBat will add a digit in this column"
 # entries with a 1 in both columns means that the classifier found passes in both classes within the .wav file-- there could be two bats in those files
 detect <- t %>% 
   filter(HiF == "1" | LoF == "1")
-
+detect[is.na(detect)] <- 0
 # this confirms that we also have the entries for which both HiF and LoF = 1 ('count' returns 715 entries of this case)
 detect %>%
   count(HiF == "1" & LoF == "1") 
 
-sum(detect$HiF == "1")
-sum(detect$LoF == "1")
+sum(detect$HiF == "1") # number of high-frequency bat passes
+sum(detect$LoF == "1") # number of low-frequency bat passes
+sum(detect$SppAccp != "0")
+# for the moment, we're going to filter out the double ones
+# no need to do this, means that there are likely two different bats in the same file, 
+# one might just not be good enough to ID past "HiF" or "LoF"
+detect$dbl <- ifelse(detect$HiF =="1" & detect$LoF == "1", "dbl", "not.dbl") #omg I did it
+detect2 <- detect %>% filter(dbl=="not.dbl")
 
 # check the results for number of files with bats
-
-nrow(detect)
 
 # (SOME OLD CODE) ---------------------------------------------------------
 ## SOME VARIABLES CALLED BELOW NOW WRONG/RENAMED...below is old code from same process as above but on just a1 (amphitheater 1)
@@ -235,18 +282,18 @@ CheckMe %>%
 
 # SPECIES RICHNESS --------------------------------------------------------
 # number of species per site
-sprich_site <- detect %>%
-  select(Site, Date, Time, AccpSpp) %>%
+sprich_site <- detect2 %>%
+  select(Site, Date, Time, SppAccp) %>%
   group_by(Site) %>%
-  summarise(sprich = n_distinct(AccpSpp)) %>%
+  summarise(sprich = n_distinct(SppAccp)) %>%
   ggplot() +
   geom_col(aes(x=Site, y=sprich))
 
 # STACKED BARPLOT SPECIES -------------------------------------------------
 # STACKED BAR - R GROUP HELP
-ggplot(detect, aes(x = Site, fill = AccpSpp)) + geom_bar()
-detect_filt <- filter(detect, AccpSpp != "x") 
-ggplot(detect_filt, aes(x = Site, fill = AccpSpp)) + geom_bar()
+ggplot(detect2, aes(x = Site, fill = SppAccp)) + geom_bar()
+detect_filt <- filter(detect2, SppAccp != "0") 
+ggplot(detect_filt, aes(x = Site, fill = SppAccp)) + geom_bar()
 # # SPECIES RICHNESS MADI'S EDITS 05012018 ----------------------------------
 # # number of species per site -- Madi's edits
 # sprich_site <- detect %>%
@@ -385,29 +432,53 @@ ggplot(detect_filt, aes(x = Site, fill = AccpSpp)) + geom_bar()
 # PLOTS -------------------------------------------------------------------
 
 # HiF and LoF over time, by Site
+# amphitheater only
+amph <- detect %>% filter(Site=="AMPHIT2") %>%
+  select(Date, Time, Site, HiF, LoF, SppAccp) %>%
+  group_by(Date) %>%
+  summarise(nHiF = sum(HiF == 1), nLoF = sum(LoF == 1))
+
+ggplot(amph) +
+  geom_point(alpha = 0.5, aes(x=Date, y=nHiF, color = "red")) +
+  geom_point(alpha = 0.5, aes(x=Date, y=nLoF, color = "blue")) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "date", 
+       y = "number of bat passes per night",
+       title = "Bat Call Activity at Amphitheater Lake") +
+  scale_colour_manual(name = NULL,
+                      values =c('red'='red','blue'='blue'), 
+                      labels = c('low-frequency', 'high-frequency')) 
 
 # detect$HiF <- as.numeric(detect$HiF)
 # detect$LoF <- as.numeric(detect$LoF)
 
 freq_group <- detect %>%
-  select(Date, Time, Site, HiF, LoF) %>%
+  select(Date, Time, hr, Site, HiF, LoF, SppAccp, Prob) %>%
+  filter(SppAccp != "0" & Prob > 0.98) 
+
+nighttally <- freq_group %>%
   group_by(Date, Site) %>%
   summarise(nHiF = sum(HiF == "1"), nLoF = sum(LoF == "1"))
   
-  ggplot(freq_group) +
+  ggplot(nighttally) +
   geom_point(alpha = 0.5, aes(x=Date, y=nHiF, color = "red")) +
   geom_point(alpha = 0.5, aes(x=Date, y=nLoF, color = "blue")) +
   facet_wrap(~Site, nrow = 3) +
   theme_minimal() + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(x = "date", 
        y = "number of bat passes per night",
-       title = "Bat Call Activity at Fishless versus Fish-containing Lakes") +
+       title = "") +
     scale_colour_manual(name = NULL,
                         values =c('red'='red','blue'='blue'), 
-                        labels = c('low-frequency', 'high-frequency')) 
+                        labels = c('low-frequency', 'high-frequency')) +
+    theme(plot.title = element_text(family = "Helvetica", size=24),
+          axis.title = element_text(family = "Helvetica", size=18), 
+          axis.text.x = element_text(family = "Helvetica", size=12, angle=50, hjust=1, vjust=1),
+          axis.text.y = element_text(family = "Helvetica", size=12, angle=0),
+          strip.text.x = element_text(size = 16))
 
-by_time <- detect %>%
+by_time <- freq_group %>%
   group_by(hr, Site) %>%
   filter(hr != 10) %>%
   summarise(nHiF = sum(HiF == "1"), nLoF = sum(LoF == "1"))
@@ -429,4 +500,23 @@ by_time_2 <- detect %>%
                         values =c('red'='red','blue'='blue'), 
                         labels = c('low-frequency', 'high-frequency')) +
     theme(legend.position = c(0.8, 0.8))
+  
+# looking at patterns by species (HUGE grain of salt right now bc no manual vetting has occurred yet)
+  
+  unique(freq_group$SppAccp)
+spp <- freq_group %>% group_by(Date, Site, SppAccp) %>%
+  summarise(daytot = n())
+
+spp %>% separate(Site, c("Basin", "Fish"), sep=-1, remove=FALSE) %>%
+  mutate(month = as.character(month(Date))) %>%
+  filter(SppAccp == "Eupe") %>%
+ggplot() +
+  geom_bar(stat="identity", position="dodge", alpha = 0.8, aes(x=Date, y=daytot, fill=SppAccp)) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "date", 
+       y = "number of bat passes per species",
+       title = "Bat Passes by Night, AMPHITHEATER") +
+ # theme(legend.position = c(0.8, 0.8)) +
+  facet_wrap(~Site, nrow=2)
 
---
+++ scripts/batexplore_20190121.R
@@ -0,0 +1,170 @@
+## code to explore SonoBat output
+## authors: M Clapp, M Boynton
+## date created: 21 Jan 2019
+## date edited: 21 Jan 2019: reads new SonoBat output generated 1/16/2019 
+
+rm(list = ls()) # this line of code removes EVERYTHING from the working environment
+
+# LOAD LIBRARIES ----------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+library(chron)
+
+# TO-DO LIST --------------------------------------------------------------
+
+
+# NEW data 1/25/2019 from Leila -------------------------------------------
+
+t <- read_csv(file = "./data/Shorts_Combined_CumSonoBatch_v430.csv")
+glimpse(t)
+# change columns with numbers to class 'numeric'
+t[,11:20] <- lapply(t[,11:20], as.numeric) # 'lapply' is taking columns 13-23 of the t dataframe and applying the function 'as.numeric' to them
+
+summary(t) #confirm that it worked-- summary can now return summary stats on the numeric columns
+t[,32:34] <- lapply(t[,32:34], as.numeric)
+
+# TEMPORARY potential solution-- changing NA's to 0's 
+t[is.na(t)] <- 0
+
+# DATE/TIME FORMATTING ----------------------------------------------------
+# create columns for date and time using the 'separate' function to split the latent info in the Filename column
+# the second 'separate' command is a janky way to do this but i don't feel like learning regular expressions rn
+t <- t %>% 
+  separate(Filename, c("Site", "Date", "Time"), sep = "_", remove = TRUE) %>%
+  separate(Time, c("Time", "fileext"), sep=".w")
+t$fileext <- NULL # this deletes this column
+
+# create a Timestamp column because it's easy to coerce to the date/time class
+t <- t %>% 
+  unite(Timestamp, Date, Time, sep = " ", remove = FALSE)
+class(t$Timestamp) 
+
+# format Timestamp to correct class
+t$Timestamp <- ymd_hms(t$Timestamp)
+class(t$Timestamp) #check class now, should be POSIXct
+
+# FINALLY create separate columns for date and time and coerce to date/time class
+t <- t %>%
+  separate(Timestamp, c("Date", "Time"), sep = " ", remove = FALSE)
+lapply(t, class) # returns class for all columns
+t$Date <- ymd(t$Date)
+t$Time <- chron(times=t$Time)
+class(t$Time)
+
+# add column for HOUR (to later investigate call activity by hour of night)
+t <- t %>%
+  mutate(hr = substr(Time, 1,2))
+
+
+# FILTER DATA -------------------------------------------------------------
+# filter out all non-bat entries and create a new dataframe called 'detect' with only entries that definitely contain bat passes
+# from SonoBat documentation: "For rudimentary tallying of bat passes, SonoBat separates bat species into low and high frequency acoustic clades. Even if unable to perform a species identification, if recognized as a bat, SonoBat will add a digit in this column"
+# entries with a 1 in both columns means that the classifier found passes in both classes within the .wav file-- there could be two bats in those files
+detect <- t %>% 
+  filter(HiF == "1" | LoF == "1")
+detect[is.na(detect)] <- 0
+# this confirms that we also have the entries for which both HiF and LoF = 1 ('count' returns 715 entries of this case)
+detect %>%
+  count(HiF == "1" & LoF == "1") 
+
+sum(detect$HiF == "1") # number of high-frequency bat passes
+sum(detect$LoF == "1") # number of low-frequency bat passes
+sum(detect$SppAccp != "0")
+# for the moment, we're going to filter out the double ones
+# no need to do this, means that there are likely two different bats in the same file, 
+# one might just not be good enough to ID past "HiF" or "LoF"
+detect$dbl <- ifelse(detect$HiF =="1" & detect$LoF == "1", "dbl", "not.dbl") #omg I did it
+detect2 <- detect %>% filter(dbl=="not.dbl")
+
+# PLOTS -------------------------------------------------------------------
+
+# HiF and LoF over time, by Site
+# amphitheater only
+amph <- detect %>% filter(Site=="AMPHIT2") %>%
+  select(Date, Time, Site, HiF, LoF, SppAccp) %>%
+  group_by(Date) %>%
+  summarise(nHiF = sum(HiF == 1), nLoF = sum(LoF == 1))
+
+ggplot(amph) +
+  geom_point(alpha = 0.5, aes(x=Date, y=nHiF, color = "red")) +
+  geom_point(alpha = 0.5, aes(x=Date, y=nLoF, color = "blue")) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "date", 
+       y = "number of bat passes per night",
+       title = "Bat Call Activity at Amphitheater Lake") +
+  scale_colour_manual(name = NULL,
+                      values =c('red'='red','blue'='blue'), 
+                      labels = c('low-frequency', 'high-frequency')) 
+
+# detect$HiF <- as.numeric(detect$HiF)
+# detect$LoF <- as.numeric(detect$LoF)
+
+freq_group <- detect %>%
+  select(Date, Time, hr, Site, HiF, LoF, SppAccp, Prob) %>%
+  filter(SppAccp != "0" & Prob > 0.98) 
+
+nighttally <- freq_group %>%
+  group_by(Date, Site) %>%
+  summarise(nHiF = sum(HiF == "1"), nLoF = sum(LoF == "1"))
+
+ggplot(nighttally) +
+  geom_point(alpha = 0.5, aes(x=Date, y=nHiF, color = "red")) +
+  geom_point(alpha = 0.5, aes(x=Date, y=nLoF, color = "blue")) +
+  facet_wrap(~Site, nrow = 3) +
+  theme_minimal() + 
+  labs(x = "date", 
+       y = "number of bat passes per night",
+       title = "") +
+  scale_colour_manual(name = NULL,
+                      values =c('red'='red','blue'='blue'), 
+                      labels = c('low-frequency', 'high-frequency')) +
+  theme(plot.title = element_text(family = "Helvetica", size=24),
+        axis.title = element_text(family = "Helvetica", size=18), 
+        axis.text.x = element_text(family = "Helvetica", size=12, angle=50, hjust=1, vjust=1),
+        axis.text.y = element_text(family = "Helvetica", size=12, angle=0),
+        strip.text.x = element_text(size = 16))
+
+by_time <- freq_group %>%
+  group_by(hr, Site) %>%
+  filter(hr != 10) %>%
+  summarise(nHiF = sum(HiF == "1"), nLoF = sum(LoF == "1"))
+
+by_time_2 <- detect %>%
+  group_by(hr) %>%
+  filter(hr != 10) %>%
+  summarise(nHiF = sum(HiF == "1"), nLoF = sum(LoF == "1"))
+
+ggplot(by_time) +
+  geom_point(alpha = 0.5, aes(x=hr, y=nHiF, color = "red")) +
+  geom_point(alpha = 0.5, aes(x=hr, y=nLoF, color = "blue")) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "hour of night (24-hour time)", 
+       y = "number of bat passes",
+       title = "Bat Passes by Hour") +
+  scale_colour_manual(name = 'acoustic clade',
+                      values =c('red'='red','blue'='blue'), 
+                      labels = c('low-frequency', 'high-frequency')) +
+  theme(legend.position = c(0.8, 0.8))
+
+# looking at patterns by species (HUGE grain of salt right now bc no manual vetting has occurred yet)
+
+unique(freq_group$SppAccp)
+spp <- freq_group %>% group_by(Date, Site, SppAccp) %>%
+  summarise(daytot = n())
+
+spp %>% separate(Site, c("Basin", "Fish"), sep=-1, remove=FALSE) %>%
+  mutate(month = as.character(month(Date))) %>%
+  filter(SppAccp == "Eupe") %>%
+  ggplot() +
+  geom_bar(stat="identity", position="dodge", alpha = 0.8, aes(x=Date, y=daytot, fill=SppAccp)) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "date", 
+       y = "number of bat passes per species",
+       title = "Bat Passes by Night, AMPHITHEATER") +
+  # theme(legend.position = c(0.8, 0.8)) +
+  facet_wrap(~Site, nrow=2)
+
